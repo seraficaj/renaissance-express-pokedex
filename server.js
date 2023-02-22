46604d9a4f4cf6b892d324e2e0b1b33cb6c34f0a
Jason Serafica <seraficaj@gmail.com>
2023-02-22 06:42:01 -0800

    explicitly set view engint to ejs

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ require("dotenv").config();
 const methodOverride = require("method-override");
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 //===SEED
 
 const PokeSeed = require("./models/pokemon.js");
 const Pokemon = require("./models/pokeSchema.js");
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
