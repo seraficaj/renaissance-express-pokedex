ee6bc552ed9e7ccde2c7014e460d1f0764ecaade
Jason Serafica <seraficaj@gmail.com>
2023-02-22 06:52:56 -0800

    add require(path)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require("path");
 const app = express();
 require("dotenv").config();
 const methodOverride = require("method-override");
