753029b98972761cb00b413b7e5928eef66c3ab0
Jason Serafica <seraficaj@gmail.com>
2023-02-22 07:30:37 -0800

    remove lines requiring path

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const path = require("path");
 const app = express();
 require("dotenv").config();
 const methodOverride = require("method-override");
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
 //===SEED
 
 const PokeSeed = require("./models/pokemon.js");
